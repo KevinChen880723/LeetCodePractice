ca610ab31d3646f735131827774fb9ccb784e1b8
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-02-10 12:15:33 +0800

    Modified Baseline Function

--- 11.ContainerWithMostWater/container.cpp
+++ 11.ContainerWithMostWater/container.cpp
@@ -1,42 +1,15 @@
 #include "container.h"
-#include "algorithm"
+#include <vector>
 
 using namespace std;
-
-class axis{
-public:
-    axis (): position(0), height(0){}
-    axis(int p, int h){
-        position = p;
-        height = h;
-    }
-    int position;
-    int height;
-};
-
-class sortAxis{
-    public:
-    bool operator()(axis a, axis b){
-        return a.height < b.height;
-    }
-};
 
 namespace baseline{
     int Solution::maxArea(vector<int>& height) {
         int i, j;
-        vector<axis> axes(height.size());
-        for (i = 0; i < height.size(); i++){
-            axes[i] = axis(i, height[i]);
-        }
-
-        // Sort the series
-        sortAxis sortObj;
-        sort(axes.begin(), axes.end(), sortObj);
-
         int maximumSize = 0;
-        for (i = 0; i < axes.size()-1; i++){
-            int currentPos = axes[i].position;
-            int currentHeight = axes[i].height;
+        for (i = 0; i < height.size(); i++){
+            int currentPos = i;
+            int currentHeight = height[i];
             int dis2Left = currentPos;
             int dis2Right = height.size()-currentPos-1;
             while (dis2Left != 0 || dis2Right != 0){
