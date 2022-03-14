127857d66a1442151d477f5fb3bcf519ab088cd0
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-03-14 12:03:31 +0800

    Modified the code

--- 141. LinkedListCycle/Solution.h
+++ 141. LinkedListCycle/Solution.h
@@ -29,12 +29,13 @@ namespace twoPointer{
         bool hasCycle(ListNode *head) {
             ListNode *slow = head;
             ListNode *fast = head;
-            if (!head || !head->next) return false;
-            while (fast->next->next) {
+            while (fast) {
+                // 本來一直在想邊界條件要怎麼設定，但其實這個很簡單，while裡面直接設定fast
+                // 之後為了避免指標在移動的時候出錯，因此先檢查fast->next有沒有東西，如果不是nullptr下一步就可以正常移動。
+                if (!fast->next) return false;
                 slow = slow->next;
                 fast = fast->next->next;
                 if (slow == fast) return true;
-                if (!fast || !fast->next) break;
             }
             return false;
         }
