7ba10c0ae91d6c05b6ece7fe042632d397b81662
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-04-22 17:34:15 +0800

    Modified the code

--- 198.HouseRobber/Solution.h
+++ 198.HouseRobber/Solution.h
@@ -10,7 +10,7 @@ public:
         int max_without_last = 0;
         for (int i: nums) {
             swap(max_with_last, max_without_last);
-            max_with_last = (max_with_last+i > max_without_last)? max_with_last+i: max_without_last;
+            max_with_last = max(max_with_last+i, max_without_last);
         }
         return max(max_without_last, max_with_last);
     }
