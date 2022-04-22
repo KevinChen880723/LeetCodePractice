7158fae9ce9bdc27719c218b79323d510059750d
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-04-22 17:08:59 +0800

    Finished the solution

---
+++ 198.HouseRobber/Solution.h
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int max_with_last = 0;
+        int max_without_last = 0;
+        for (int i: nums) {
+            swap(max_with_last, max_without_last);
+            max_with_last = (max_with_last+i > max_without_last)? max_with_last+i: max_without_last;
+        }
+        return max(max_without_last, max_with_last);
+    }
+};
