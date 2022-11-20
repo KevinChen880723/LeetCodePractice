101fe857097512b7e2bcaa9d15753105ab0469f8
KevinChen880723 <b10602120@mail.ntust.edu.tw>
2022-11-20 12:08:35 +0800

    Finish 253

--- 253.MeetingRoomsII/Solution.cpp
+++ 253.MeetingRoomsII/Solution.cpp
@@ -21,20 +21,20 @@ public:
         for (int i = 0; i < intervals.size()-1; i++) {
             if (intervals[i].end >= intervals[i+1].start) {
                 meetingsHavntEnd.push_back(intervals[i].end);
-            }
-            vector<int> temp;
-            int currentOverlapped = 0;
-            for (int j = 0; j < meetingsHavntEnd.size(); j++) {
-                if (meetingsHavntEnd[j] > intervals[i+1].start) {
-                    currentOverlapped++;
-                    temp.push_back(meetingsHavntEnd[j]);
+                vector<int> temp;
+                int currentOverlapped = 0;
+                for (int j = 0; j < meetingsHavntEnd.size(); j++) {
+                    if (meetingsHavntEnd[j] > intervals[i+1].start) {
+                        currentOverlapped++;
+                        temp.push_back(meetingsHavntEnd[j]);
+                    }
+                    else { 
+                        currentOverlapped--;
+                    }
                 }
-                else { 
-                    currentOverlapped--;
-                }
+                meetingsHavntEnd = temp;
+                MaxOverlapped = max(MaxOverlapped, currentOverlapped);
             }
-            meetingsHavntEnd = temp;
-            MaxOverlapped = max(MaxOverlapped, currentOverlapped);
         }
         return MaxOverlapped+1;
     }
